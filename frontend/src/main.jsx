c3dd1d54c72a485c2e31e6f35eed5db6a2f480c5
JoaoRLFranco <joaolemefranco@gmail.com>
2025-11-08 02:26:00 -0400

    sla

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/navbar";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './pages/Home'
-import Cadastro from './pages/Pedido/Cadastro'
+import Cadastro from "./pages/Pedido/Cadastro";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,11 +12,9 @@ createRoot(document.getElementById("root")).render(
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-                  <Route path="/pedido/cadastro" element={<Cadastro />} />
-
+          <Route path="/pedido/cadastro" element={<Cadastro />} />
         </Routes>
       </Router>
     </div>
   </StrictMode>
 );
-    
